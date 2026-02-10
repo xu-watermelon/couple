919da910500a34a1560b2fa6ca337751ed63bf6d
xuwatermelon <xuwatermelon@gamail.com>
2026-02-10 17:50:55 +0800

    完成用户信息查询功能 - 实现getUserInfo接口 - 添加权限校验防止越权访问 - 支持数据脱敏和友好错误提示

--- src/test/java/com/xkb/couple/CoupleApplicationTests.java
+++ src/test/java/com/xkb/couple/CoupleApplicationTests.java
@@ -2,7 +2,6 @@ package com.xkb.couple;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class CoupleApplicationTests {
